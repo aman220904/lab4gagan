4dc003f4e490498f118d62b2f19281afb186bbbb
aman <amandeep321.231@gmail.com>
2024-11-25 13:22:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lab4gagan/ChatRoomViewModel.java
@@ -0,0 +1,11 @@
+
+package com.example.lab4gagan;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import java.util.ArrayList;
+
+public class ChatRoomViewModel extends ViewModel {
+
+    // LiveData to hold the list of chat messages
+    public MutableLiveData<ArrayList<ChatMessage>> messages = new MutableLiveData<>(new ArrayList<>());
+}
---
+++ app/src/main/java/com/example/lab4gagan/FirstActivity.java
@@ -0,0 +1,4 @@
+package com.example.lab4gagan;
+
+public class FirstActivity {
+}
